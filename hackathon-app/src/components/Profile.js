60dc7dec72722a145b27e4027f33cad57e9c155c
Peter Hagenson <phagenso@travelers.com>
2023-02-08 16:38:00 -0500

    add required data displayed

--- hackathon-app/src/components/Profile.js
+++ hackathon-app/src/components/Profile.js
@@ -1,9 +1,19 @@
+//import Search from './Search.js'
+
 
 function Profile(){
     const user = JSON.parse(localStorage.getItem("user"));
     console.log(user);
     return (
-        <p>Welcome to the Profile Page</p>
+        <>
+        <h3>Welcome to your profile, {user.name}!</h3>
+        <p>Employee Name: {user.name}</p>
+        <p>Location: {user.location}</p>
+        <p>Role: {user.role}</p>
+        <p>Phone Number: {user.phone}</p>
+        <p>Salary: ${user.Salary}</p>
+        </>
+
     )
 }
 
