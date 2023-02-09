ccc81ecde458f190bc0e0f559cd20d728d52efcd
Bob Smith <wasadmin@webagesolutions.com>
2023-02-09 15:33:31 -0500

    changes

--- hackathon-app/src/components/Profile.js
+++ hackathon-app/src/components/Profile.js
@@ -1,17 +1,33 @@
+import { borderBottom } from '@mui/system';
 import Search from './Search.js'
 
+const lastProfileElement = {
+    paddingBottom: '10px',
+    borderBottom: 'solid'
+}
+
+const headingDiv = {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+  
 
 function Profile(){
     const user = JSON.parse(localStorage.getItem("user"));
     console.log(user);
     return (
         <>
+        <div style={headingDiv}>
         <h3>Welcome to your profile, {user.name}!</h3>
-        <p>Employee Name: {user.name}</p>
-        <p>Location: {user.location}</p>
-        <p>Role: {user.role}</p>
-        <p>Phone Number: {user.phone}</p>
-        <p>Salary: ${user.Salary}</p>
+        </div>
+       
+        <div>Employee Name: {user.name}</div>
+        
+        <div>Location: {user.location}</div>
+        <div>Role: {user.role}</div>
+        <div>Phone Number: {user.phone}</div>
+        <div style={lastProfileElement}>Salary: ${user.Salary}</div>
+        
         <Search />
         </>
 
