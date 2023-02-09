ec750be7a155ac15b2ff1ea877edfe99d5b6ce99
Bob Smith <wasadmin@webagesolutions.com>
2023-02-09 11:45:51 -0500

    added some style to login page

--- hackathon-app/src/components/Login.js
+++ hackathon-app/src/components/Login.js
@@ -1,6 +1,19 @@
 import {useState} from 'react'
 import {useNavigate} from 'react-router-dom'
+import Button from '@mui/material/Button'
 
+const styles1 = {
+    backgroundColor: "black",
+    color: "white",
+    border: "3px solid black",
+    '&:hover': {
+      border: "3px solid black",
+      color: "black",
+      backgroundColor: "white",
+      fontWeight: 'bold',
+      // boxShadow: 20
+    }
+  }
 
 function Login(){
 
@@ -8,6 +21,7 @@ function Login(){
     let [password, setPassword] = useState('')
     //let [user, setUser] = useState('')
     let navigate = useNavigate();
+
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -36,14 +50,31 @@ async function getUser(){
  }
     return ( 
         <>
-        <p>Welcome to the Login Page</p>
+        <h2>Welcome to the Enterprise Directory!</h2>
+        <div style={cardStyle}>
+        <h4>Please log in to continue</h4>
         <form onSubmit={handleSubmit}>
+            <label>Username:
+                <br></br>
             <input type="text" name="username" value={userName} onChange={(e)=>setUserName(e.target.value)}></input>
+            </label>
+            <br></br>
+            <label>Password:
+                <br></br>
             <input type="text" name="password" value={password}onChange={(e)=>setPassword(e.target.value)} ></input>
-            <button type="submit">Login</button>
+            </label>
+            <br></br>
+            <Button sx={styles1} type="submit">Login</Button>
         </form>
+        </div>
         </>
     )
+}
+
+const cardStyle = {
+    borderStyle: 'solid',
+    borderWidth: 'thin',
+    width: '300px'
 }
 
 export default Login
